45c33e63899e6be0765e20f385ca0d349a3cc1e5
Cristian Rimbu <cristian.rimbu@devinterface.com>
2025-05-07 08:57:35 +0200

    feat: update environment configuration and add railway deployment settings

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // await app.listen(3000);
+  // Railway espone process.env.PORT
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 App is running on http://localhost:${port}`);
 }
 bootstrap();
