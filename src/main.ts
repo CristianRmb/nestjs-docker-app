3451b54662e95fd2dfa55d193124718471c4231f
Cristian Rimbu <cristian.rimbu@devinterface.com>
2025-05-08 15:03:11 +0200

    feat: implement global exception filter and enhance user creation logging

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,11 +8,10 @@ async function bootstrap() {
     origin: '*', // Puoi specificare l'URL del frontend se lo desideri
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Aggiungi i metodi che ti servono
   });
+  app.useGlobalFilters(new HttpExceptionFilter());
   // await app.listen(3000);
   // Railway espone process.env.PORT
   const port = process.env.PORT || 8080;
-
-  console.log('process.env.PORT', process.env.PORT);
 
   await app.listen(port, '0.0.0.0');
   console.log(`🚀 App is running on http://localhost:${port}`);
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -9,7 +9,8 @@ export class UsersController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    const resp = this.usersService.create(createUserDto);
+    return resp;
   }
 
   @Get('/all')
